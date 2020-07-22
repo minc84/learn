10a545f2a00bce5968a0a8013c45d80b5d6ce2c4
Nikolay Nesterovich <nesterovish@yandex.ru>
2020-07-22 17:18:14 +0300

    error 404, flash page

--- app.py
+++ app.py
@@ -23,15 +23,27 @@ def login1():
 	return render_template("login.html", menu=menu)
 
 @app.route('/form', methods=['POST', 'GET'])
+
 def form():
+	
 	if request.method == 'POST':
-		print(request.form['username'])
+		if len(request.form['username']) > 2:
+			flash('сообщение отправлена', category='succes')
+		else:
+			flash('Ошибка', category='error')
+
 	return render_template("form.html", menu=menu)
 
 @app.route("/profile/<int:username>/<path>")
 def profile(username, path):
     return f"Пользователь: {username},{path}"
 
+@app.errorhandler(404)
+
+def pageNotFount(error):
+ 	return render_template('page404.html', menu=menu)
+
 if __name__ == '__main__':
       app.run(host=os.getenv('IP', '127.0.0.1'),
             port=int(os.getenv('PORT', 4000)))
+      
