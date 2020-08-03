2c0adef1880f32140ebc60d8b3a66d860c8d693d
Nikolay Nesterovich <nesterovish@yandex.ru>
2020-08-03 17:11:49 +0300

    db

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask
 from config import Configuration
 import os
-from flask import render_template, flash, redirect, url_for, request, session, abort
+from flask import render_template, flash, redirect, url_for, request, session, abort, g
+import sqlite3
+from FDataBase import FDataBase
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
 
+def connect_db():
+	conn=sqlite3.connect(app.config['DATABASE'])
+	conn.row_factory = sqlite3.Row 
+	return conn
+
+def create_db():
+	db = connect_db()
+	with app.open_resource('sq_db.sql', mode='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
+	db.close()
+
+def get_db():
+	if not hasattr(g, 'link_db'):
+		g.link_db = connect_db()
+	return g.link_db
+
+@app.teardown_appcontext
+def close(error):
+		if hasattr(g, 'link_db'):
+			g.link_db.close()
 
 
 
@@ -14,10 +37,13 @@ menu = [{'name': 'Главная', 'url': '/'},
 		{'name': 'Логин', 'url': 'login'},
 		{'name': 'Форма', 'url': 'form'}]
 
+
 @app.route('/')
 def index():
-	print(url_for('index'))
-	return render_template("index.html", menu=menu)
+	db = get_db()
+	dbase = FDataBase(db)
+	
+	return render_template("index.html", menu=dbase.getMenu())
 
 @app.route('/form', methods=['POST', 'GET'])
 
