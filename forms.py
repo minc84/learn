5b572f69a9357fe01393bb8529cb69ef7c47b1f3
Nikolay Nesterovich <nesterovish@yandex.ru>
2020-12-24 15:51:22 +0300

    wtf form label error

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
  
 class LoginForm(FlaskForm):
-	email = StringField("Email: ", validators=[Email()])
-	psw = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=4, max=100)])
+	email = StringField("Email: ", validators=[Email("Некорректный email")])
+	psw = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=4, max=100, message="Пароль должен быть от 4 до 100 символов")])
 	remember = BooleanField("Запомнить", default = False)
 	submit = SubmitField("Войти")
