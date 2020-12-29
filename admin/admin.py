05ba773ee67493efb179fd6903892134383f0249
Nikolay Nesterovich <nesterovish@yandex.ru>
2020-12-29 17:11:11 +0300

    blueprind db

--- admin/admin.py
+++ admin/admin.py
@@ -1,7 +1,80 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, url_for, flash, session, redirect, g
+import sqlite3
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
+
+db = None
+@admin.before_request
+
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global db
+    db = g.get('link_db')
+ 
+@admin.teardown_request
+def teardown_request(request):
+	global db
+	db = None
+	return request
+
+
+
+menu = [{'url': '.index', 'title': 'Панель'},
+		{'url': '.listpubs', 'title': 'Список статей'},
+        {'url': '.logout', 'title': 'Выйти'}]
+
+
+def login_admin():
+	session['admin_logged'] = 1
+
+def isLogged():
+	return True if session.get('admin_logged') else False
+ 
+def logout_admin():
+	session.pop('admin_logged', None)
+
+
+@admin.route('/login', methods=["POST", "GET"])
+def login():
+	if request.method == "POST":
+		if request.form['user'] == "admin" and request.form['psw'] == "12345":
+			login_admin()
+			return redirect(url_for('.index'))
+		else:
+			flash("Неверная пара логин/пароль", "error")
+ 
+	return render_template('admin/login.html', title='Админ-панель')
+
+@admin.route('/logout', methods=["POST", "GET"])
+def logout():
+	if not isLogged():
+		return redirect(url_for('.login'))
+ 
+	logout_admin()
+ 
+	return redirect(url_for('.login'))
+
 @admin.route('/')
 def index():
-	return "admin"
+	if not isLogged():
+		return redirect(url_for('.login'))
+ 
+	return render_template('admin/index.html', menu=menu, title='Админ-панель')
+
+
+@admin.route('/list-pubs')
+def listpubs():
+	if not isLogged():
+		return redirect(url_for('.login'))
+ 
+	list = []
+	if db:
+		try:
+			cur = db.cursor()
+			cur.execute(f"SELECT title, text, url FROM posts")
+			list = cur.fetchall()
+		except sqlite3.Error as e:
+			print("Ошибка получения статей из БД " + str(e))
+ 
+	return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=list)
